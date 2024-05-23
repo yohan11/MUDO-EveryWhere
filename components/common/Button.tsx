ac91b43b118b5b8b312f89c8025dc7ab32513486
yohan11 <yipco77@naver.com>
2024-05-24 03:49:02 +0900

    feat: 등록 버튼 UI 구현 및 storybook 연결

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -11,6 +11,11 @@ const PrimaryButton = ({ text }: TButtonText) => {
   return <button className="bg-mainbrown px-5 py-2 rounded-2xl text-white">{text}</button>;
 };
 
+const SubButton = ({ text }: TButtonText) => {
+  return <button className="bg-gray-300 px-5 py-2 rounded-3xl">{text}</button>;
+};
+
 Button.Primary = PrimaryButton;
+Button.Sub = SubButton;
 
 export default Button;
--- stories/PrimaryButton.stories.ts
+++ stories/PrimaryButton.stories.ts
@@ -4,7 +4,7 @@ import Button from '@components/common/Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Button/Primary',
+  title: 'Button/PrimaryButton',
   component: Button.Primary,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
