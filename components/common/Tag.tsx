a83bafbb53fafe1f76fdb5a27a063e5b4ab6c81d
yohan11 <yipco77@naver.com>
2024-05-24 01:50:56 +0900

    feat: 각진 태그 UI 구현 및 storybook 연결

--- components/common/Tag.tsx
+++ components/common/Tag.tsx
@@ -13,6 +13,21 @@ const RoundTag = ({ text, selected }: ITag) => {
   return <div className={selected ? 'rounded-3xl bg-neutral-200 px-5 py-2' : ''}>{text}</div>;
 };
 
+const SquareTag = ({ text, selected }: ITag) => {
+  return (
+    <div
+      className={
+        selected
+          ? 'rounded-2xl bg-mainblue text-white px-5 py-2'
+          : 'rounded-2xl border-solid border border-gray-300 px-5 py-2'
+      }
+    >
+      {text}
+    </div>
+  );
+};
+
 Tag.Round = RoundTag;
+Tag.Square = SquareTag;
 
 export default Tag;
