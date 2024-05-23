3b96081659bcd556275a9ce1d15a4e256794a43e
yohan11 <yipco77@naver.com>
2024-05-24 07:38:44 +0900

    refactor: 상수 분리

--- components/home/TagList.tsx
+++ components/home/TagList.tsx
@@ -1,22 +1,7 @@
 import React from 'react';
-import memberIcon from '@public/icon/member.svg';
-import sceneIcon from '@public/icon/scene.svg';
-import moodIcon from '@public/icon/mood.svg';
-import weatherIcon from '@public/icon/weather.svg';
 import Tag from '@components/common/Tag';
 import Image from 'next/image';
-
-const PARENT_CATEGORY = [
-  { icon: memberIcon, text: '멤버' },
-  { icon: sceneIcon, text: '상황' },
-  {
-    icon: moodIcon,
-    text: '기분',
-  },
-  { icon: weatherIcon, text: '날씨' },
-];
-
-const MEMBER_CATEGORY = ['유재석', '박명수', '정준하', '정형돈', '노홍철', '하하', '길'];
+import { MEMBER_CATEGORY, PARENT_CATEGORY } from '@constants/category';
 
 export const ParentTagList = () => {
   return (
---
+++ constants/category.ts
@@ -0,0 +1,16 @@
+import memberIcon from '@public/icon/member.svg';
+import sceneIcon from '@public/icon/scene.svg';
+import moodIcon from '@public/icon/mood.svg';
+import weatherIcon from '@public/icon/weather.svg';
+
+export const PARENT_CATEGORY = [
+  { icon: memberIcon, text: '멤버' },
+  { icon: sceneIcon, text: '상황' },
+  {
+    icon: moodIcon,
+    text: '기분',
+  },
+  { icon: weatherIcon, text: '날씨' },
+];
+
+export const MEMBER_CATEGORY = ['유재석', '박명수', '정준하', '정형돈', '노홍철', '하하', '길'];
